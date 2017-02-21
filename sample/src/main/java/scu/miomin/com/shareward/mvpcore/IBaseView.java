cb292e7c771a20e9f85b1b57cfca5eb6b7416435
miomin <691292118@qq.com>
2017-02-21 12:30:07 +0800

    update

--- sample/src/main/java/scu/miomin/com/shareward/mvpcore/IBaseView.java
+++ sample/src/main/java/scu/miomin/com/shareward/mvpcore/IBaseView.java
@@ -5,9 +5,21 @@ package scu.miomin.com.shareward.mvpcore;
  */
 public interface IBaseView {
 
+    /**
+     * 显示加载动画
+     * @param title 标题
+     * @param message 内容
+     */
     void showLoading(String title, String message);
 
+    /**
+     * 隐藏加载动画
+     */
     void hideLoading();
 
+    /**
+     * 生成Toast
+     * @param message 内容
+     */
     void showToast(String message);
 }
